630302d72db49ff57bec69b00ab38ea0954b62d4
ozvaris <ozgurozvaris@hotmail.com>
2020-08-11 18:21:35 +0300

    next codes ignored

--- app.js
+++ app.js
@@ -10,16 +10,16 @@ const app = express();
 
 console.log(process.env.JWT_KEY);
 
-mongoose
-  .connect(
-    "mongodb+srv://max:" + process.env.MONGO_ATLAS_PW + "@clustertest-vy4wh.mongodb.net/test"
-  )
-  .then(() => {
-    console.log("Connected to database!");
-  })
-  .catch(() => {
-    console.log("Connection failed!");
-  });
+// mongoose
+//   .connect(
+//     "mongodb+srv://max:" + process.env.MONGO_ATLAS_PW + "@clustertest-vy4wh.mongodb.net/test"
+//   )
+//   .then(() => {
+//     console.log("Connected to database!");
+//   })
+//   .catch(() => {
+//     console.log("Connection failed!");
+//   });
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
