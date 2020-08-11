b4ee5ae19e5a7880a2ab2ee4f09c0e0a9127001c
ozvaris <ozgurozvaris@hotmail.com>
2020-08-11 18:42:03 +0300

    next codes ignored

--- app.js
+++ app.js
@@ -8,38 +8,33 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-console.log(process.env.JWT_KEY);
+mongoose
+  .connect(
+    "mongodb+srv://max:" + process.env.MONGO_ATLAS_PW + "@clustertest-vy4wh.mongodb.net/test"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://max:" + process.env.MONGO_ATLAS_PW + "@clustertest-vy4wh.mongodb.net/test"
-//   )
-//   .then(() => {
-//     console.log("Connected to database!");
-//   })
-//   .catch(() => {
-//     console.log("Connection failed!");
-//   });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("images")));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use("/images", express.static(path.join("images")));
-
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-//   );
-//   next();
-// });
-
-console.log("haaa!");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
 app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
